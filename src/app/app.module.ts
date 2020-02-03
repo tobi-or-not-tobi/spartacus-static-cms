521fc117b738baef09d140172651b3b5b834ddde
Tobias Ouwejan <tobias.ouwejan@sap.com>
2020-02-03 21:55:35 +0100

    Add ^1.0.0 spartacus to new app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,35 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserModule } from '@angular/platform-browser';
+import { translationChunksConfig, translations } from '@spartacus/assets';
+import { CmsLibModule, StorefrontModule } from '@spartacus/storefront';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StorefrontModule.withConfig({
+      backend: {
+        occ: {
+          baseUrl:
+            'https://storefront.c39j2-walkersde1-d4-public.model-t.cc.commerce.ondemand.com',
+        },
+      },
+      context: {
+        baseSite: ['electronics-spa'],
+      },
+      i18n: {
+        resources: translations,
+        chunks: translationChunksConfig,
+        fallbackLang: 'en',
+      },
+      features: {
+        level: '1.4',
+      },
+    }),
+    CmsLibModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
